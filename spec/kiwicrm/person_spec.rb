b3168068348b481833c42faa77b3a78b7776f0a3
Piotr Szotkowski <chastell@chastell.net>
2011-01-06 17:42:31 +0100

    fix Person.new spec to actually make sense

--- spec/kiwicrm/person_spec.rb
+++ spec/kiwicrm/person_spec.rb
@@ -3,7 +3,9 @@ module KiwiCRM describe Person do
   describe '.new' do
 
     it 'allows for setting arbitrary properties' do
-      People.id('librarian').given_names.first.should == 'Horace'
+      bengo = Person.new given_names: ['Bengo'], surname: 'Macarona'
+      bengo.title = 'Professor'
+      bengo.given_names.first.should == 'Bengo'
     end
 
   end
