4fa842a0a575a150ebb2e52baf8afe3138378c05
Piotr Szotkowski <chastell@chastell.net>
2011-01-06 10:54:38 +0100

    putting the Librarian where he belongs

--- spec/kiwicrm/people_spec.rb
+++ spec/kiwicrm/people_spec.rb
@@ -3,7 +3,7 @@ module KiwiCRM describe People do
   describe '.all' do
 
     it 'returns an Enumerator with all the Person objects' do
-      librarian = Person.new name_prefix: 'Dr', given_names: ['Horace'], surname: 'Worblehat'
+      librarian = Person.new YAML.load_file('spec/fixtures/people.yml').first
       People.all.should be_an Enumerator
       People.all.to_a.should == [librarian]
     end
--- spec/kiwicrm/person_spec.rb
+++ spec/kiwicrm/person_spec.rb
@@ -3,7 +3,7 @@ module KiwiCRM describe Person do
   describe '.new' do
 
     it 'allows for setting arbitrary properties' do
-      librarian = Person.new name_prefix: 'Dr', given_names: ['Horace'], surname: 'Worblehat'
+      librarian = Person.new YAML.load_file('spec/fixtures/people.yml').first
       librarian.given_names.first.should == 'Horace'
     end
 
