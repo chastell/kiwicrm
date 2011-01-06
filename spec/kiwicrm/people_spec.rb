507d6f63008a55f6f91c9f715ba84e11262645a7
Piotr Szotkowski <chastell@chastell.net>
2011-01-06 13:25:43 +0100

    refactor People spec

--- spec/kiwicrm/people_spec.rb
+++ spec/kiwicrm/people_spec.rb
@@ -1,13 +1,16 @@
 module KiwiCRM describe People do
 
+  before do
+    @archchancellor = Person.new YAML.load_file('spec/fixtures/people.yml').first
+    @librarian      = Person.new YAML.load_file('spec/fixtures/people.yml').last
+  end
+
   describe '.all' do
 
     it 'returns an Enumerator with all the Person objects' do
-      archchancellor = Person.new YAML.load_file('spec/fixtures/people.yml').first
-      librarian      = Person.new YAML.load_file('spec/fixtures/people.yml').last
       People.all.should be_an Enumerator
-      People.all.should include archchancellor
-      People.all.should include librarian
+      People.all.should include @archchancellor
+      People.all.should include @librarian
       People.all.to_a.size.should == 2
     end
 
@@ -16,10 +19,8 @@ module KiwiCRM describe People do
   describe '.find' do
 
     it 'returns the first Person fulfilling the requirements' do
-      archchancellor = Person.new YAML.load_file('spec/fixtures/people.yml').first
-      librarian      = Person.new YAML.load_file('spec/fixtures/people.yml').last
-      People.find { |p| p.surname == 'Ridcully'  }.should == archchancellor
-      People.find { |p| p.surname == 'Worblehat' }.should == librarian
+      People.find { |p| p.surname == 'Ridcully'  }.should == @archchancellor
+      People.find { |p| p.surname == 'Worblehat' }.should == @librarian
     end
 
   end
