7bee5d4abd0b5d5ab3894b1f4afbdd0ac8143677
Piotr Szotkowski <chastell@chastell.net>
2011-01-08 07:04:48 +0100

    workaroundy bandaid

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ RSpec.configure do |config|
 
   config.before :all do
     YAML.load_file('spec/fixtures/people.yml').each do |hash|
-      KiwiCRM::Person.new hash
+      KiwiCRM::Person.new hash unless KiwiCRM::People.id hash['id']
     end
   end
 
