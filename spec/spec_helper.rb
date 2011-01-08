ed601adbdf2a9fa4cb504c966508d64580190383
Piotr Szotkowski <chastell@chastell.net>
2011-01-08 09:34:11 +0100

    once and for all

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,8 @@
-RSpec.configure do |config|
+YAML.load_file('spec/fixtures/people.yml').each do |hash|
+  KiwiCRM::Person.new hash
+end
 
-  config.before :all do
-    YAML.load_file('spec/fixtures/people.yml').each do |hash|
-      KiwiCRM::Person.new hash unless KiwiCRM::People.id hash[:id]
-    end
-    YAML.load_file('spec/fixtures/rels.yml').each do |hash|
-      hash[:refs].map! { |ref| {ref: KiwiCRM.const_get(ref.first), id: ref.last} }
-      KiwiCRM::Rel.new hash
-    end
-  end
-
+YAML.load_file('spec/fixtures/rels.yml').each do |hash|
+  hash[:refs].map! { |ref| {ref: KiwiCRM.const_get(ref.first), id: ref.last} }
+  KiwiCRM::Rel.new hash
 end
