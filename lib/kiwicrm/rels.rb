852484eea9a311a807388422e4dc02b0e8f5d321
Piotr Szotkowski <chastell@chastell.net>
2011-01-09 10:51:09 +0100

    untagle Rels from People

--- lib/kiwicrm/rels.rb
+++ lib/kiwicrm/rels.rb
@@ -3,7 +3,7 @@ module KiwiCRM module Rels
   extend self, EntityPool
 
   def of object
-    select { |rel| rel.refs.include?({ref: People, id: object.id}) }
+    select { |rel| rel.refs.include?({ref: object.class.entity_pool, id: object.id}) }
   end
 
 end end
