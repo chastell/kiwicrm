2c05a69b4d7658e16fc2471f3531cb7ebd982e35
Piotr Szotkowski <chastell@chastell.net>
2011-01-10 23:11:08 +0100

    introducing EntityRels#{rels}

--- lib/kiwicrm/entity_rels.rb
+++ lib/kiwicrm/entity_rels.rb
@@ -1,7 +1,19 @@
 module KiwiCRM class EntityRels < Array
 
   def initialize object
-    super Rels.select { |rel| rel.refs.include?({ref: object.class.entity_pool, id: object.id}) }
+    @ref = {ref: object.class.entity_pool, id: object.id}
+    super Rels.select { |rel| rel.refs.include? @ref }
+    @rels = group_by do |rel|
+      rel.rels rescue next
+      case @ref
+      when rel.refs.first then rel.rels.last
+      when rel.refs.last  then rel.rels.first
+      end
+    end.each_value { |rels| rels.map! { |rel| rel.refs - [@ref] }.flatten!.uniq! }
+  end
+
+  def method_missing method, *args, &block
+    @rels[method] or super
   end
 
 end end
--- spec/kiwicrm/entity_rels_spec.rb
+++ spec/kiwicrm/entity_rels_spec.rb
@@ -11,4 +11,21 @@ module KiwiCRM describe EntityRels do
 
   end
 
+  describe '#{rels}' do
+
+    it 'return refs pointed by given rels' do
+      sams       = EntityRels.new People.id :sam
+      sybils     = EntityRels.new People.id :sybil
+      young_sams = EntityRels.new People.id :young_sam
+
+      sams.spouses.should    == [{ref: People, id: :sybil}]
+      sybils.spouses.should  == [{ref: People, id: :sam}]
+      sams.children.should   == [{ref: People, id: :young_sam}]
+      sybils.children.should == [{ref: People, id: :young_sam}]
+      young_sams.parents.should include({ref: People, id: :sam})
+      young_sams.parents.should include({ref: People, id: :sybil})
+    end
+
+  end
+
 end end
