0346e7a359f95c1012cf9f4956bd64118fc6fb0b
Piotr Szotkowski <chastell@chastell.net>
2011-01-19 22:56:01 +0100

    un-Array EntityRels

--- lib/kiwicrm/entity_rels.rb
+++ lib/kiwicrm/entity_rels.rb
@@ -1,8 +1,9 @@
-module KiwiCRM class EntityRels < Array
+module KiwiCRM class EntityRels
 
   def initialize object
-    super Rels.select { |rel| rel.refs? object }
-    @rels = group_by { |rel| rel.rel_to_other object }.each_value { |rels| rels.map! { |rel| rel.other_ref object } }
+    @rels = Rels.select { |rel| rel.refs? object }
+                .group_by { |rel| rel.rel_to_other object }
+                .each_value { |rels| rels.map! { |rel| rel.other_ref object } }
   end
 
   def method_missing method, *args, &block
--- spec/kiwicrm/entity_rels_spec.rb
+++ spec/kiwicrm/entity_rels_spec.rb
@@ -1,16 +1,5 @@
 module KiwiCRM describe EntityRels do
 
-  describe '.new' do
-
-    it 'creates a store of Rel objects referencing the parameter' do
-      rel = Rel.new refs: [{ref: People, id: :young_sam}, {ref: People, id: :sam}], rels: [:students, :teachers]
-      sams = EntityRels.new People.id :sam
-      sams.should include rel
-      sams.size.should == 3
-    end
-
-  end
-
   describe '#{rels}' do
 
     it 'return refs pointed by given rels' do
